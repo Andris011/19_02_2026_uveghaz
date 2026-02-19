1d35248b65024b8a9d74bbeb3cdd26494bd22d0e
Andris011 <andris.febert111@gmail.com>
2026-02-19 14:52:26 +0100

    Kreate Kezelo.cs

--- Kezelo.cs
+++ Kezelo.cs
@@ -2,5 +2,37 @@
 
 public class Kezelo
 {
-    
+    private string nev;
+    private string azonosito;
+    private Szerepkor szerepkor;
+
+    public Kezelo(string nev, string azonosito, Szerepkor szerepkor)
+    {
+        this.nev = nev;
+        this.azonosito = azonosito;
+        this.szerepkor = szerepkor;
+    }
+
+    public string Nev
+    {
+        get => nev;
+        set => nev = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Azonosito
+    {
+        get => azonosito;
+        set => azonosito = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Szerepkor Szerepkor
+    {
+        get => szerepkor;
+        set => szerepkor = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.nev}: {this.azonosito}";
+    }
 }
